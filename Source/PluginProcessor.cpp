1e8409f868567f02ab38036d13595f3522258e68
Pechorin <albermorser0@gmail.com>
2025-10-16 21:35:40 +0500

    Refactored PluginProcessor.cpp/.h

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -4,148 +4,115 @@
 //==============================================================================
 PanCakeAudioProcessor::PanCakeAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       ),
-        apvts(*this, nullptr, "Parameters", createParameterLayout()),
-        pitchDetector(3 * 2048, 512),
-        ringBuffer(pitchDetector.getFrameSize() * 4),
-        pitchThread(pitchDetector, ringBuffer)
+    : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+                         ),
+      apvts(*this, nullptr, "Parameters", createParameterLayout()), pitchDetector(3 * 2048, 512),
+      ringBuffer(pitchDetector.getFrameSize() * 4), pitchThread(pitchDetector, ringBuffer)
 #endif
 {
 }
 
-PanCakeAudioProcessor::~PanCakeAudioProcessor()
-{
-}
+PanCakeAudioProcessor::~PanCakeAudioProcessor() {}
 
 //==============================================================================
-const juce::String PanCakeAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
+const juce::String PanCakeAudioProcessor::getName() const { return JucePlugin_Name; }
 
 bool PanCakeAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool PanCakeAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool PanCakeAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
-double PanCakeAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
+double PanCakeAudioProcessor::getTailLengthSeconds() const { return 0.0; }
 
-int PanCakeAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
+int PanCakeAudioProcessor::getNumPrograms() { return 1; }
 
-int PanCakeAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
+int PanCakeAudioProcessor::getCurrentProgram() { return 0; }
 
-void PanCakeAudioProcessor::setCurrentProgram (int index)
-{
-}
+void PanCakeAudioProcessor::setCurrentProgram(int index) {}
 
-const juce::String PanCakeAudioProcessor::getProgramName (int index)
-{
-    return {};
-}
+const juce::String PanCakeAudioProcessor::getProgramName(int index) { return {}; }
 
-void PanCakeAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+void PanCakeAudioProcessor::changeProgramName(int index, const juce::String &newName) {}
 
 //==============================================================================
-void PanCakeAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void PanCakeAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    juce::dsp::ProcessSpec spec;
-    
-    spec.maximumBlockSize = samplesPerBlock;
-    spec.sampleRate = sampleRate;
-    spec.numChannels = getTotalNumOutputChannels();
-    
     pitchThread.prepare();
     pitchDetector.prepare(sampleRate, 0.15);
 }
 
-void PanCakeAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
+void PanCakeAudioProcessor::releaseResources() {}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool PanCakeAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool PanCakeAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+        layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+#if !JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 #endif
 
-void PanCakeAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void PanCakeAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer,
+                                         juce::MidiBuffer &midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-    
+        buffer.clear(i, 0, buffer.getNumSamples());
+
     const juce::ScopedLock sl(bufferLock);
     sharedBuffer.makeCopyOf(buffer);
-    
+
     fillRingBuffer(buffer);
-    
-    if (auto* playHead = getPlayHead())
+
+    if (auto *playHead = getPlayHead())
     {
         if (auto pos = playHead->getPosition())
         {
@@ -156,27 +123,24 @@ void PanCakeAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
 }
 
 //==============================================================================
-bool PanCakeAudioProcessor::hasEditor() const
-{
-    return true;
-}
+bool PanCakeAudioProcessor::hasEditor() const { return true; }
 
-juce::AudioProcessorEditor* PanCakeAudioProcessor::createEditor()
+juce::AudioProcessorEditor *PanCakeAudioProcessor::createEditor()
 {
-    return new PanCakeAudioProcessorEditor (*this);
+    return new PanCakeAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void PanCakeAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void PanCakeAudioProcessor::getStateInformation(juce::MemoryBlock &destData)
 {
     juce::MemoryOutputStream mos(destData, true);
     apvts.state.writeToStream(mos);
 }
 
-void PanCakeAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void PanCakeAudioProcessor::setStateInformation(const void *data, int sizeInBytes)
 {
     auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
-    
+
     if (tree.isValid())
     {
         apvts.replaceState(tree);
@@ -185,69 +149,54 @@ void PanCakeAudioProcessor::setStateInformation (const void* data, int sizeInByt
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new PanCakeAudioProcessor();
-}
-
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() { return new PanCakeAudioProcessor(); }
 
 juce::AudioProcessorValueTreeState::ParameterLayout PanCakeAudioProcessor::createParameterLayout()
 {
     using namespace juce;
-    
+
     AudioProcessorValueTreeState::ParameterLayout layout;
-    
-    layout.add(std::make_unique<juce::AudioParameterFloat>( ParameterID("MIX", 1),
-                                                            "Mix",
-                                                            juce::NormalisableRange<float>(0.f, 100.f, 1.f, 1.f),
-                                                            100.f));
-    
-    layout.add(std::make_unique<juce::AudioParameterChoice>(ParameterID("WAVETYPE", 1),
-                                                            "Wave Type",
-                                                            juce::StringArray {"Sine", "Triangle"},
-                                                            0
-                                                            ));
-    
-    layout.add(std::make_unique<juce::AudioParameterFloat>(ParameterID("HERTZRATE", 1),
-                                                           "Speed",
-                                                           juce::NormalisableRange<float>(0.f, 100.f, 0.01f, 0.4f),
-                                                           0));
-    
-    juce::StringArray modeChoices = {"Hertz Retrig.", "Beat Retrig.", "Hertz Synced", "Beat Synced", "Pitch -> Rate"};
-    layout.add(std::make_unique<juce::AudioParameterChoice>(ParameterID("MODE", 1),
-                                                            "Mode",
-                                                            modeChoices,
-                                                            1));
-    
-    juce::StringArray noteDurationChoices = {
-        "32 Bars", "16 Bars", "8 Bars", "4 Bars",
-        "3 Bars", "2 Bars", "1.5 Bars", "1 Bar",
-        "3/4", "1/2", "3/8", "1/4", "3/16", "1/8", "1/16",
-        "1/32", "1/64", "1/128"
-    };
-    layout.add(std::make_unique<juce::AudioParameterChoice>(ParameterID("DURATION", 1),
-                                                            "Note Duration",
-                                                            noteDurationChoices,
-                                                            10));
-    
+
+    layout.add(std::make_unique<AudioParameterFloat>(
+        ParameterID("MIX", 1), "Mix", NormalisableRange<float>(0, 100.f, 1.f, 1.f), 100.f));
+
+    layout.add(std::make_unique<AudioParameterChoice>(ParameterID("WAVETYPE", 1), "Wave Type",
+                                                      StringArray{"Sine", "Triangle"}, 0));
+
+    layout.add(std::make_unique<AudioParameterFloat>(
+        ParameterID("HERTZRATE", 1), "Speed", NormalisableRange<float>(0, 100.f, 0.01f, 0.4f), 0));
+
+    StringArray modeChoices = {"Hertz Retrig.", "Beat Retrig.", "Hertz Synced", "Beat Synced",
+                               "Pitch -> Rate"};
+    layout.add(
+        std::make_unique<AudioParameterChoice>(ParameterID("MODE", 1), "Mode", modeChoices, 1));
+
+    StringArray noteDurationChoices = {
+        "32 Bars", "16 Bars", "8 Bars", "4 Bars", "3 Bars", "2 Bars", "1.5 Bars", "1 Bar", "3/4",
+        "1/2",     "3/8",     "1/4",    "3/16",   "1/8",    "1/16",   "1/32",     "1/64",  "1/128"};
+    layout.add(std::make_unique<AudioParameterChoice>(ParameterID("DURATION", 1), "Note Duration",
+                                                      noteDurationChoices, 10));
+
     return layout;
 }
 
-void PanCakeAudioProcessor::fillRingBuffer(juce::AudioBuffer<float> &buffer)
+void PanCakeAudioProcessor::fillRingBuffer(const juce::AudioBuffer<float> &buffer)
 {
     const int numSamples = buffer.getNumSamples();
+    auto *left = buffer.getReadPointer(0);
+    auto *right = buffer.getReadPointer(1);
+
     if (buffer.getNumChannels() > 1)
     {
         std::vector<float> mono(numSamples);
-        
+
         for (int i = 0; i < numSamples; i++)
         {
-            mono[i] = (buffer.getReadPointer(0)[i] + buffer.getReadPointer(1)[i]) * 0.5;
+            mono[i] = (left[i] + right[i]) / 2;
         }
-        
+
         ringBuffer.push(mono.data(), numSamples);
     }
     else
-        ringBuffer.push(buffer.getReadPointer(0), numSamples);
+        ringBuffer.push(left, numSamples);
 }
-
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -3,32 +3,30 @@
 #include <JuceHeader.h>
 
 #include "Panner.h"
-#include "YinFFT.h"
-#include "RingBuffer.h"
 #include "PitchThread.h"
+#include "RingBuffer.h"
+#include "YinFFT.h"
 
 
-/**
-*/
 class PanCakeAudioProcessor : public juce::AudioProcessor
 {
-public:
+  public:
     //==============================================================================
     PanCakeAudioProcessor();
     ~PanCakeAudioProcessor() override;
 
     //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
+#ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
+#endif
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
     //==============================================================================
-    juce::AudioProcessorEditor* createEditor() override;
+    juce::AudioProcessorEditor *createEditor() override;
     bool hasEditor() const override;
 
     //==============================================================================
@@ -42,29 +40,29 @@ public:
     //==============================================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+    void setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void changeProgramName(int index, const juce::String &newName) override;
 
     //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
-    
-    const Panner& getPanner() const { return panner; }
-    
+    void getStateInformation(juce::MemoryBlock &destData) override;
+    void setStateInformation(const void *data, int sizeInBytes) override;
+
+    const Panner &getPanner() const { return panner; }
+
     juce::AudioProcessorValueTreeState apvts;
     juce::AudioBuffer<float> sharedBuffer;
     juce::CriticalSection bufferLock;
 
-private:
+  private:
     YinFFT pitchDetector;
     RingBuffer ringBuffer;
     PitchDetectionThread pitchThread;
     Panner panner;
-    
+
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    void fillRingBuffer(juce::AudioBuffer<float>& buffer);
-    
+    void fillRingBuffer(const juce::AudioBuffer<float> &buffer);
+
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PanCakeAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PanCakeAudioProcessor)
 };
