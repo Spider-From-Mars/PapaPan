806270b0be06c32696306a1b22f8cd13575d2653
Pechorin <albermorser0@gmail.com>
2025-10-18 15:12:12 +0500

    Refactored PluginEditor.cpp/.h

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,34 +1,22 @@
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
 
-//==============================================================================
-PanCakeAudioProcessorEditor::PanCakeAudioProcessorEditor (PanCakeAudioProcessor& p) :
-    AudioProcessorEditor (&p),
-    audioProcessor (p),
-    basePanel(audioProcessor.apvts),
-    plot(p)
+PanCakeAudioProcessorEditor::PanCakeAudioProcessorEditor(PanCakeAudioProcessor &p)
+    : AudioProcessorEditor(&p), audioProcessor(p), basePanel(audioProcessor.apvts), plot(p)
 {
     addAndMakeVisible(basePanel);
     addAndMakeVisible(plot);
-        
+
     setSize(530, 630);
 }
 
-PanCakeAudioProcessorEditor::~PanCakeAudioProcessorEditor()
-{
-}
+PanCakeAudioProcessorEditor::~PanCakeAudioProcessorEditor() {}
 
 //==============================================================================
-void PanCakeAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    g.fillAll(BaseColours::basePink);
-}
+void PanCakeAudioProcessorEditor::paint(juce::Graphics &g) { g.fillAll(BaseColours::basePink); }
 
 void PanCakeAudioProcessorEditor::resized()
 {
     basePanel.setBounds(0, 0, 220, getHeight());
-    plot.setBounds(basePanel.getWidth(),
-                   0,
-                   getWidth() - basePanel.getWidth() - 8,
-                   getHeight());
+    plot.setBounds(basePanel.getWidth(), 0, getWidth() - basePanel.getWidth() - 8, getHeight());
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,31 +1,26 @@
 #pragma once
 
-#include <JuceHeader.h>
-#include "PluginProcessor.h"
+#include "BaseColours.h"
 #include "BasePanel.h"
 #include "PlotComponent.h"
-#include "BaseColours.h"
+#include "PluginProcessor.h"
+#include <JuceHeader.h>
 
-//==============================================================================
-/**
-*/
-class PanCakeAudioProcessorEditor  : public juce::AudioProcessorEditor
+class PanCakeAudioProcessorEditor : public juce::AudioProcessorEditor
 {
-public:
-    PanCakeAudioProcessorEditor (PanCakeAudioProcessor&);
+  public:
+    PanCakeAudioProcessorEditor(PanCakeAudioProcessor &);
     ~PanCakeAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics &) override;
     void resized() override;
 
-private:
-    PanCakeAudioProcessor& audioProcessor;
-    
+  private:
+    PanCakeAudioProcessor &audioProcessor;
+
     BasePanel basePanel;
     PlotComponent plot;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PanCakeAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PanCakeAudioProcessorEditor)
 };
-
-
